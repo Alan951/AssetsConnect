b2d6b3b97b433b673dcbf733a98ebfc12e1f87d9
Jorge Alan <jorge_951-ck@hotmail.com>
2015-09-19 02:09:37 -0500

    Modificacion controlador login

---
+++ src/java/SQLdb/DbConnection.java
@@ -0,0 +1,13 @@
+
+package SQLdb;
+
+/**
+ *
+ * @author Jorge Alan Villalón Pérez
+ */
+public class DbConnection {
+    static String bd;
+    static String login;
+    static String password;
+    static String url;
+}
